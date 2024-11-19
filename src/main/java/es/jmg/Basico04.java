b098ca244ca2a71ec5693e7e8b7423cb13b56b0f
Juanma <Juanma888@gmail.com>
2024-11-19 17:08:31 +0100

    Tests unitarios OK

--- src/main/java/es/jmg/Basico04.java
+++ src/main/java/es/jmg/Basico04.java
@@ -14,7 +14,7 @@ package es.jmg;
  */
 public class Basico04 {
     // private. Varriables quedan encapsuladas. 
-    // Si queremos acceder a ellas desde un método estático=> variables también estáticas
+    // Si queremos acceder a ellas desde un método estático => variables también estáticas
     private static int a;
     private static int b;
     private static int c;
@@ -25,8 +25,31 @@ public class Basico04 {
         b = (55+9) % 9;
         c = 20 + -3*5 / 8;
         d = 5 + 15 / 3 * 2 - 8 % 3;
-
+    // void no devuelve ningún valor. Ver a través del depurador
     }
+    // Al estar encapsuladas las variables, es preciso usar Getters para acceder a ellas.
+    // Así pues, para los tests son imprescindibles.
+    // Btn_Dcho.\Source Action -> Generar Getters & Setters
+    
+
+    public static int getA() {
+        return a;
+    }
+
+    public static int getB() {
+        return b;
+    }
+
+    public static int getC() {
+        return c;
+    }
+
+    public static int getD() {
+        return d;
+    }
+
+
+
     public static void main(String[] args) {
         // Prohibido usar System.out.println(); Ahh!. Usar puntos de ruptura y depurar.
         calcularSeries();
